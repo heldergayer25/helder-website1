c6499d903c9cf4ea134a0524b81ef2dc5cc54ee8
helderwebergayer@gmail.com <helderwebergayer@gmail.com@fe378a3e-3d8b-3946-376f-2a8003a5fb41>
2014-07-11 20:45:28 +0000

    Utilização do yoxview, faltando implementar a galeria dentro do portfólio, conflito de classes css

--- trunk/Sheila/referencias.php
+++ trunk/Sheila/referencias.php
@@ -9,16 +9,16 @@
 <script src="js/modernizr.js" type="text/javascript"></script>
 <script type="text/javascript" src="js/jquery-1.4.2.js"></script>
 <script src="js/config.js" type="text/javascript"></script>
-<script src="js/cufon.js" type="text/javascript"></script>
-<script src="js/portfolio.jquery.js" type="text/javascript"></script>
-<script src="js/gulpfile.js" type="text/javascript"></script>
+<script src="js/plugins/cufon/cufon.js" type="text/javascript"></script>
+<script src="js/plugins/portfolio/portfolio.jquery.js" type="text/javascript"></script>
+<script src="js/plugins/portfolio/gulpfile.js" type="text/javascript"></script>
 <script type="text/javascript" src="js/plugins/yoxview-gallery/jquery.yoxview-2.21.min.js"></script>
 
 <link rel="icon" type="image/x-icon" href="imagens/favicon.ico" />
 
 <!-- Fontes -->
-<script src="js/Daisy_Script_400.font.js" type="text/javascript"></script>
-<script src="js/Open_Sans_400.font.js" type="text/javascript"></script>
-<script src="js/Josefin_Sans_400.font.js" type="text/javascript"></script>
-<script src="js/Josefin_Sans_Semi_Bold_600.font (1).js" type="text/javascript"></script>
-<script src="js/Indie_Flower_400.font.js" type="text/javascript"></script>
+<script src="js/fontes/Daisy_Script_400.font.js" type="text/javascript"></script>
+<script src="js/fontes/Open_Sans_400.font.js" type="text/javascript"></script>
+<script src="js/fontes/Josefin_Sans_400.font.js" type="text/javascript"></script>
+<script src="js/fontes/Josefin_Sans_Semi_Bold_600.font (1).js" type="text/javascript"></script>
+<script src="js/fontes/Indie_Flower_400.font.js" type="text/javascript"></script>
