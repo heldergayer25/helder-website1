38ea082c335754bd6304e5ade82670fcd1dbb3a6
helderwebergayer@gmail.com <helderwebergayer@gmail.com@fe378a3e-3d8b-3946-376f-2a8003a5fb41>
2014-07-18 22:52:15 +0000

    Netbook

--- Sheila/cabecalho.php
+++ Sheila/cabecalho.php
@@ -3,11 +3,11 @@
         <ul>
             <li><a href="http://instagram.com/" title="Instagram" id="instagram" target="_blank">Instagram</a></li>
             <li><a href="https://twitter.com/" title="Twitter" id="twitter" target="_blank">Twitter</a></li>
-            <li><a href="http://www.youtube.com/" title="YouTube" id="youtube" target="_blank">YouTube</a></li>
+            <li><a href="http://www.youtube.com/?gl=BR&hl=pt" title="YouTube" id="youtube" target="_blank">YouTube</a></li>
             <li><a href="https://www.facebook.com/" title="Facebook" id="facebook" target="_blank">Facebook</a></li>
         </ul>
     </nav>        
-    <h1><a href="index.php" title="Desenhando Moda">Desenhando moda</a></h1>        
+    <h1><a href="#" title="Desenhando Moda"></a></h1>        
     <nav id="menu">
         <ul>
             <li><a href="#" title="Home" id="home">Home</a></li>
@@ -15,7 +15,7 @@
             <li><a href="loja.php" title="Loja" id="loja">Loja</a></li>
             <li><a href="portfolio.php" title="Portfólio" id="portfolio">Portfólio</a></li>
             <li><a href="contato.php" title="Contato" id="contato">Contato</a></li>
-            <!--<li><a href="anuncie.php" title="Anuncie" id="anuncie">Anuncie</a></li>-->            
+            <li><a href="anuncie.php" title="Anuncie" id="anuncie">Anuncie</a></li>
         </ul>
     </nav>
 </header>
