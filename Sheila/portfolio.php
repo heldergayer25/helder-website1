41df77898477cd9b0980ede31bc22d55557120c4
helderwebergayer@gmail.com <helderwebergayer@gmail.com@fe378a3e-3d8b-3946-376f-2a8003a5fb41>
2014-07-23 00:17:59 +0000

    Capas do portfólio colocadas em sequência correta

--- Sheila/portfolio.php
+++ Sheila/portfolio.php
@@ -19,25 +19,25 @@ and open the template in the editor.
                         <a href="#thumb1" class="thumbnail" style="background-image: url('imagens/portfolio/capa/croqui.jpg')">
                         <h4>Croquis</h4><span class="description">Croquis</span></a>
                     </li>
-                    <li><a href="#thumb2" class="thumbnail" style="background-image: url('imagens/portfolio/capa/cartoes-comemorativos.jpg')">
-                        <h4>Cartões</h4><span class="description">Cartões Comemorativos</span></a>
-                    </li>                    
                     <li>
-                        <a href="#thumb3" class="thumbnail" style="background-image: url('imagens/portfolio/capa/desenho-tecnico.jpg')">
+                        <a href="#thumb2" class="thumbnail" style="background-image: url('imagens/portfolio/capa/desenho-tecnico.jpg')">
                         <h4>Desenhos Técnicos</h4><span class="description">Desenhos Técnicos</span></a>
                     </li>
                     <li>
-                        <a href="#thumb4" class="thumbnail" style="background-image: url('imagens/portfolio/capa/estampa.jpg')">
+                        <a href="#thumb3" class="thumbnail" style="background-image: url('imagens/portfolio/capa/estampa.jpg')">
                         <h4>Estampas</h4><span class="description">Estampas</span></a>
+                    </li>
+                    <li>
+                        <a href="#thumb4" class="thumbnail" style="background-image: url('imagens/portfolio/capa/material-grafico.jpg')">
+                        <h4>Material Gráfico</h4><span class="description">Material Gráfico</span></a>
                     </li>
                     <li>
                         <a href="#thumb5" class="thumbnail" style="background-image: url('imagens/portfolio/capa/infografico.jpg')">
                         <h4>Infográfico</h4><span class="description">Infográfico</span></a>
                     </li>
-                    <li>
-                        <a href="#thumb6" class="thumbnail" style="background-image: url('imagens/portfolio/capa/material-grafico.jpg')">
-                        <h4>Material Gráfico</h4><span class="description">Material Gráfico</span></a>
-                    </li>
+                    <li><a href="#thumb6" class="thumbnail" style="background-image: url('imagens/portfolio/capa/cartoes-comemorativos.jpg')">
+                        <h4>Cartões</h4><span class="description">Cartões Comemorativos</span></a>
+                    </li>                    
                 </ul>       
                 
                 <div class="portfolio-content">                    
@@ -72,17 +72,8 @@ and open the template in the editor.
                                 <a class="fancybox-effects-a" href="imagens/portfolio/original/03.jpg" data-fancybox-group="gallery" title="Lorem ipsum dolor sit amet"><img src="imagens/portfolio/thumbnail/03.jpg" alt="" /></a>
                             </p>                                                
                             <!--<a href="#" class="btn btn-primary">Learn More</a>-->
-                    </div>      
+                    </div>   
                     <div id="thumb2">                        
-                        <h1>Cartões</h1>
-                        <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse potenti. Integer a posuere tortor. Praesent malesuada mauris massa, non blandit neque tempus nec. Quisque fermentum nunc non hendrerit tempus.</p>
-                        <p class="galeria">
-                            <a class="fancybox-effects-b" href="imagens/portfolio/original/01.jpg" data-fancybox-group="gallery" title="Lorem ipsum dolor sit amet"><img src="imagens/portfolio/thumbnail/01.jpg" alt="" /></a>
-                            <a class="fancybox-effects-b" href="imagens/portfolio/original/02.jpg" data-fancybox-group="gallery" title="Lorem ipsum dolor sit amet"><img src="imagens/portfolio/thumbnail/02.jpg" alt="" /></a>
-                            <a class="fancybox-effects-b" href="imagens/portfolio/original/03.jpg" data-fancybox-group="gallery" title="Lorem ipsum dolor sit amet"><img src="imagens/portfolio/thumbnail/03.jpg" alt="" /></a>
-                        </p>  
-                    </div>
-                    <div id="thumb3">                        
                         <h1>Desenhos Técnicos</h1>
                         <p>Ut condimentum eros bibendum metus lacinia, ac condimentum justo faucibus. Nam nec dui convallis, sodales sapien in, gravida justo. Pellentesque pulvinar massa a nisl iaculis, quis iaculis elit tincidunt.</p>                       
                         <p class="galeria">
@@ -91,13 +82,22 @@ and open the template in the editor.
                             <a class="fancybox-effects-c" href="imagens/portfolio/original/03.jpg" data-fancybox-group="gallery" title="Lorem ipsum dolor sit amet"><img src="imagens/portfolio/thumbnail/03.jpg" alt="" /></a>
                         </p>  
                     </div>
-                    <div id="thumb4">                        
+                    <div id="thumb3">                        
                         <h1>Estampas</h1>
                         <p>Suspendisse cursus commodo elit, at tempus felis hendrerit vel. Cras condimentum aliquam mauris at blandit. Pellentesque ac velit iaculis, lobortis nibh id, ultricies ante. Fusce vel urna justo. Maecenas rhoncus vel ligula eget feugiat. Maecenas blandit risus eros, vel imperdiet augue dapibus vitae.</p>                        
                         <p class="galeria">
                             <a class="fancybox-effects-d" href="imagens/portfolio/original/01.jpg" data-fancybox-group="gallery" title="Lorem ipsum dolor sit amet"><img src="imagens/portfolio/thumbnail/01.jpg" alt="" /></a>
                             <a class="fancybox-effects-d" href="imagens/portfolio/original/02.jpg" data-fancybox-group="gallery" title="Lorem ipsum dolor sit amet"><img src="imagens/portfolio/thumbnail/02.jpg" alt="" /></a>
                             <a class="fancybox-effects-d" href="imagens/portfolio/original/03.jpg" data-fancybox-group="gallery" title="Lorem ipsum dolor sit amet"><img src="imagens/portfolio/thumbnail/03.jpg" alt="" /></a>
+                        </p>  
+                    </div>
+                    <div id="thumb4">                        
+                        <h1>Material Gráfico</h1>
+                        <p>Vestibulum gravida, ante ut consectetur dictum, dolor sapien pretium elit, sed tincidunt augue sem a lorem. Vivamus quis neque pharetra, consequat dolor vel, venenatis urna. Praesent diam quam, fermentum vel tortor eget, pulvinar tincidunt velit.</p>                        
+                        <p class="galeria">
+                            <a class="fancybox-thumbs" href="imagens/portfolio/original/01.jpg" data-fancybox-group="thumb" title="Lorem ipsum dolor sit amet"><img src="imagens/portfolio/thumbnail/01.jpg" alt="" /></a>
+                            <a class="fancybox-thumbs" href="imagens/portfolio/original/02.jpg" data-fancybox-group="thumb" title="Lorem ipsum dolor sit amet"><img src="imagens/portfolio/thumbnail/02.jpg" alt="" /></a>
+                            <a class="fancybox-thumbs" href="imagens/portfolio/original/03.jpg" data-fancybox-group="thumb" title="Lorem ipsum dolor sit amet"><img src="imagens/portfolio/thumbnail/03.jpg" alt="" /></a>
                         </p>  
                     </div>
                     <div id="thumb5">                        
@@ -110,12 +110,12 @@ and open the template in the editor.
                         </p>  
                     </div>   
                     <div id="thumb6">                        
-                        <h1>Material Gráfico</h1>
-                        <p>Vestibulum gravida, ante ut consectetur dictum, dolor sapien pretium elit, sed tincidunt augue sem a lorem. Vivamus quis neque pharetra, consequat dolor vel, venenatis urna. Praesent diam quam, fermentum vel tortor eget, pulvinar tincidunt velit.</p>                        
+                        <h1>Cartões</h1>
+                        <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse potenti. Integer a posuere tortor. Praesent malesuada mauris massa, non blandit neque tempus nec. Quisque fermentum nunc non hendrerit tempus.</p>
                         <p class="galeria">
-                            <a class="fancybox-thumbs" href="imagens/portfolio/original/01.jpg" data-fancybox-group="thumb" title="Lorem ipsum dolor sit amet"><img src="imagens/portfolio/thumbnail/01.jpg" alt="" /></a>
-                            <a class="fancybox-thumbs" href="imagens/portfolio/original/02.jpg" data-fancybox-group="thumb" title="Lorem ipsum dolor sit amet"><img src="imagens/portfolio/thumbnail/02.jpg" alt="" /></a>
-                            <a class="fancybox-thumbs" href="imagens/portfolio/original/03.jpg" data-fancybox-group="thumb" title="Lorem ipsum dolor sit amet"><img src="imagens/portfolio/thumbnail/03.jpg" alt="" /></a>
+                            <a class="fancybox-effects-b" href="imagens/portfolio/original/01.jpg" data-fancybox-group="gallery" title="Lorem ipsum dolor sit amet"><img src="imagens/portfolio/thumbnail/01.jpg" alt="" /></a>
+                            <a class="fancybox-effects-b" href="imagens/portfolio/original/02.jpg" data-fancybox-group="gallery" title="Lorem ipsum dolor sit amet"><img src="imagens/portfolio/thumbnail/02.jpg" alt="" /></a>
+                            <a class="fancybox-effects-b" href="imagens/portfolio/original/03.jpg" data-fancybox-group="gallery" title="Lorem ipsum dolor sit amet"><img src="imagens/portfolio/thumbnail/03.jpg" alt="" /></a>
                         </p>  
                     </div>
                 </div>
