b4e6de3878930990c10e2de8797e8c756a0d8389
helderwebergayer@gmail.com <helderwebergayer@gmail.com@fe378a3e-3d8b-3946-376f-2a8003a5fb41>
2014-08-26 02:27:00 +0000

    Recaptcha instalado e validando form de e-mail

--- trunk/Sheila/contato.php
+++ trunk/Sheila/contato.php
@@ -9,6 +9,15 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title>Contato</title>
         <?php include './referencias.php';?>
+        <script type="text/javascript">
+            var RecaptchaOptions = {                
+               //Temas 
+               //'red' | 'white' | 'blackglass' | 'clean' | 'custom'
+               theme : 'white',               
+               //linguagem
+               lang : 'pt'
+            };
+        </script>
     </head>
     <body>                
         <?php include'./cabecalho.php'?>       
@@ -16,24 +25,31 @@ and open the template in the editor.
             <section class="wrapper">  
                 <div class="texto">
                     <p>Se está interessado(a) no meu trabalho ou tem dúvidas, sinta-se à vontade para me contactar, través do formulário abaixo:</p>
-                    <form action="sistema/phpmailer/email.php" id="formContato" name="formContato" method="post">
-                    <fieldset>                        
-                        <label for="txNome">Nome: <input id="txNome" name="txNome" type="text" /></label>
-                        <label for="txEmail">E-mail: <input id="txEmail" name="txEmail" type="text" /></label>
-                        <label for="txTelefone">Telefone: <input id="txTelefone" name="txTelefone" type="text" /></label>                                            
-                        <label id="slAssunto">
-                            Assunto:
-                            <select id="slAssunto" name="slAssunto">
-                                <option value="...">...</option>
-                                <option value="Orçamento">Orçamento</option>
-                                <option value="Dúvidas">Dúvidas</option>
-                            </select>
-                        </label>
-                        <label for="txDescricao">Descrição: <textarea id="txDescricao" name="txDescricao" type="textarea" cols="22" rows="7"></textarea></label>
-                    </fieldset>
-                        <br/>                        
-                    <input type="submit" id="sbEnviar" name="sbEnviar" value="Enviar" />                    
-                </form>
+                    <form action="sistema/recaptcha/verificar.php" id="formContato" name="formContato" method="post">                                            
+                        <fieldset>                        
+                            <label for="txNome">Nome: <input id="txNome" name="txNome" type="text" /></label>
+                            <label for="txEmail">E-mail: <input id="txEmail" name="txEmail" type="text" /></label>
+                            <label for="txTelefone">Telefone: <input id="txTelefone" name="txTelefone" type="text" /></label>                                            
+                            <label id="slAssunto">
+                                Assunto:
+                                <select id="slAssunto" name="slAssunto">
+                                    <option value="...">...</option>
+                                    <option value="Orçamento">Orçamento</option>
+                                    <option value="Dúvidas">Dúvidas</option>
+                                </select>
+                            </label>
+                            <label for="txDescricao">Descrição: <textarea id="txDescricao" name="txDescricao" type="textarea" cols="22" rows="7"></textarea></label>
+                        </fieldset>                            
+                    <div class="clear"></div>
+                    <br/>
+                    <?php
+                        require_once ('sistema/recaptcha/recaptchalib.php');
+                        $publickey = "6LfjD_kSAAAAAA-BpQ5Wey8-T4K1HpH3iIP_1i1H";  // chave pública
+                        echo recaptcha_get_html($publickey);
+                    ?>                        
+                    <br/>                        
+                    <input type="submit" id="sbEnviar" name="sbEnviar" value="Enviar"  />                    
+                </form>                    
                 </div> 
             </section>        
         <div id="section_home"></div>
---
+++ trunk/Sheila/sistema/recaptcha/verificar.php
@@ -0,0 +1,34 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require_once('../recaptcha/recaptchalib.php');
+require("../phpmailer/class.phpmailer.php");
+require '../phpmailer/email.php';
+  $privatekey = "6LfjD_kSAAAAACYlzi22Gk8cdeCU9e_jW8gldwRP";
+  $resp = recaptcha_check_answer ($privatekey,
+                                $_SERVER["REMOTE_ADDR"],
+                                $_POST["recaptcha_challenge_field"],
+                                $_POST["recaptcha_response_field"]);
+
+  if (!$resp->is_valid) {
+    echo "<meta charset='UTF-8'><script>alert('Código de segurança digitado errado, por favor tente novamente!'); javascript:window.history.go(-1);</script>";
+  } else {    
+    
+    $nome = $_POST["txNome"];
+    $email = $_POST["txEmail"];
+    $telefone = $_POST["txTelefone"];
+    $assunto = $_POST["slAssunto"];
+    $descricao = $_POST["txDescricao"];
+
+    $enviar = new email();
+  
+    $enviar->validarEmail($nome, $email, $telefone, $assunto, $descricao);
+      
+  }
+  
+  
