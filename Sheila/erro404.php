43029e65d559a8b300034cbb6fb0147d182b48e8
helderwebergayer@gmail.com <helderwebergayer@gmail.com@fe378a3e-3d8b-3946-376f-2a8003a5fb41>
2014-09-05 01:01:19 +0000

    Página erro 404 finalizada

--- Sheila/erro404.php
+++ Sheila/erro404.php
@@ -11,19 +11,14 @@ and open the template in the editor.
         <?php include './referencias.php';?>
     </head>
     <body>        
-         <?php include'./cabecalho.php'?>       
-        <div id="section_home">
-               
-            <section class="wrapper">
-               
-                <div id="erro404">
-                    
-                </div> 
-                
-            </section>
-        
-        </div>        
-        
+        <?php include'./cabecalho.php'?>      
+        <div id="section_home"></div>    
+        <div id="erro404">
+            <section class="wrapper">                  
+                <div id="fantasma"></div>
+            </section>   
+        </div>
+        <div id="section_home"></div>
         <?php include './rodape.php';?>
     </body>
 </html>
