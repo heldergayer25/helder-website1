d90ab0f55afbc0ac699dd2f67b81540687e5afa2
helderwebergayer@gmail.com <helderwebergayer@gmail.com@fe378a3e-3d8b-3946-376f-2a8003a5fb41>
2014-09-09 23:37:04 +0000

    Página perfil concluída

--- trunk/Sheila/perfil.php
+++ trunk/Sheila/perfil.php
@@ -16,16 +16,16 @@ and open the template in the editor.
             <section class="wrapper">                  
                 <img id="perfil1" src="imagens/perfil1.jpg" alt="Sheila Cristina Alves de Arruda" />       
                 <div class="perfil1" style="background-color: #FFF; width: 450px; height: 413px; float: right; z-index: -2; position: relative;">
-                    <p>Sheila Cristina Alves de Arruda, apelido: Shee.
-25 anos, Curitibana, formada em Designer de MOda.
-Designer freelancer nas áreas de moda e gráfico.</p>
+                    <p id="nome">Sheila Cristina Alves de Arruda,</p> 
+                    <p>apelido: Shee. 25 anos, Curitibana, formada em Designer de Moda.
+                        Designer freelancer nas áreas de moda e gráfico.</p>
                 </div>
                 <div class="perfil2" style="background-color: #E9556A; float: left; width: 450px; height: 429px;">
                     <p>Admiradora do mundo da moda, maquiagem, 
-esmalte, produtos de beleza, livros, desenhos, 
-danças, video game, cinema, séries, toys, música,
-shows, rock and roll, viagens e muito mais 
-muuuito chocolate.</p>
+                        esmalte, produtos de beleza, livros, desenhos, 
+                        dança, video game, cinema, séries, toys, música,
+                        shows, rock and roll, viagens e muito mas 
+                        muuuito chocolate.</p>
                 </div>
                 <img id="perfil2" src="imagens/perfil2.jpg" alt="Sheila Cristina Alves de Arruda" />                                
                 <div class="clear"></div>
