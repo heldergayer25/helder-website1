bae0e457b5f19e21736285ad7da0ea07ebf1714c
helderwebergayer@gmail.com <helderwebergayer@gmail.com@fe378a3e-3d8b-3946-376f-2a8003a5fb41>
2014-09-10 00:33:53 +0000

    Página perfil concluída

--- Sheila/rodape.php
+++ Sheila/rodape.php
@@ -1,12 +1,9 @@
 <footer>
     <div class="wrapper">
-        <address>Sheila Cristina Alves de Arruda - Designer de Moda<br/>Curitiba - PR</address>       
-        
-        <div id="assinatura_rodape"></div>
-        
-        <div id="cartoon_rodape"></div>
-        
+        <address>Sheila Cristina Alves de Arruda - Designer de Moda<br/>Curitiba - PR</address>               
+        <div id="assinatura_rodape"></div>        
+        <div id="cartoon_rodape"></div>        
         <p>2014® Todos os direitos reservados.</p>
-        
+        <div class="clear"></div>
     </div>
 </footer>
