9892faa52dba620b3b163b89e99df10f7dd70070
helderwebergayer@gmail.com <helderwebergayer@gmail.com@fe378a3e-3d8b-3946-376f-2a8003a5fb41>
2014-09-20 03:32:38 +0000

    Primeiro post - Estilo dos Posts

--- trunk/Sheila/index.php
+++ trunk/Sheila/index.php
@@ -19,13 +19,49 @@ and open the template in the editor.
                 <h2>EM BREVE, NOVIDADES NO BLOG! </h2>
             </div>
             </section> -->           
-        <div id="apresentacao_home">            
+        <!--<div id="apresentacao_home">            
             <h2>EM BREVE, NOVIDADES NO BLOG! </h2>            
-        </div>            
-        <section class="wrapper">
-            <div class="fb-like" data-href="http://desenhandomoda.com.br/" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+        </div>-->        
+        <section class="wrapper">                       
+            <div class="broche"></div>            
+            <div class="posts-titulo">Look - Jardim Botânico</div>                
+            <div class="posts">               
+               <p>Olá pessoal, meu primeiro post uhul!
+                Estou muito animada com o projeto
+                do blog! Tenho muitas ideias legais,
+                divertidas, e muito loucas para mostrar
+                a vocês, espero que curtam.</p>
+                <img src="imagens/posts/fotos/photo1.JPG" />                
+                <p>Então vamos começar...No último final de 
+                semana fui ao Jardim Botânico, adoro esse
+                parque, e quando sai um solzinho por aqui
+                tem que aproveitar ao máximo, pois Curitiba
+                é literalmente uma cidade bipolar hehe.
+                Então resolvi vestir uma batinha bem
+                fresquinha, uma calça detonada e meu
+                querido all star.</p>                
+                <img src="imagens/posts/fotos/photo2.JPG" />
+                <img src="imagens/posts/fotos/photo3.JPG" />
+                <img src="imagens/posts/fotos/photo4.JPG" />
+                <img src="imagens/posts/fotos/photo5.JPG" />
+                <img src="imagens/posts/fotos/photo6.JPG" />
+                <img src="imagens/posts/fotos/photo7.JPG" />
+                <img src="imagens/posts/fotos/photo8.JPG" />
+                <img src="imagens/posts/fotos/photo9.JPG" />
+                <img src="imagens/posts/fotos/photo10.JPG" />
+                <img src="imagens/posts/fotos/photo11.JPG" />
+                <img src="imagens/posts/fotos/photo12.JPG" />
+                <p>Croqui - Reprodução do look Jardim Botânico</p>
+                <img src="imagens/posts/fotos/photo13.JPG" />                
+                <p>Bom galera é isso, vou compartilhar um
+                pouco do meu mundo com vocês, das
+                coisas que eu amo e curto em fazer.
+                Espero que gostem e até o próximo post!</p>                
+                <div class="rodape-posts"></div>
+                <div class="fb-like" data-href="http://desenhandomoda.com.br/" data-layout="standard" data-action="like" data-show-faces="true" data-share="true" style="margin-top: 40px;"></div>                
+            </div>     
             <!--<div class="fb-like" data-href="http://desenhandomoda.com.br/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>-->
-            <div class="fb-comments" data-href="http://desenhandomoda.com.br/" data-numposts="1" data-colorscheme="light"></div>
+            <div class="fb-comments" data-href="http://desenhandomoda.com.br/" data-numposts="1" data-colorscheme="light" data-width="640"></div>
         </section>        
         <div id="section_home"></div>
         <?php include './rodape.php';?>
