c01d04527d3f743d7cc5a2f336a5aafcf8425b5a
helderwebergayer@gmail.com <helderwebergayer@gmail.com@fe378a3e-3d8b-3946-376f-2a8003a5fb41>
2014-10-08 00:18:45 +0000

    Terceiro post Paginação

---
+++ trunk/Sheila/pagina2.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Desenhando Moda</title>
+        <?php include './referencias.php';?>
+    </head>
+    <body>                
+        <div id="fb-root"></div>
+        <?php include'./cabecalho.php'?>       
+        <div id="section_home"></div>     
+            <!--<section class="wrapper">
+                <div class="texto" id="novidades">
+                <h2>EM BREVE, NOVIDADES NO BLOG! </h2>
+            </div>
+            </section> -->                   
+        <section class="wrapper">        
+            <div class="broche"></div>   
+            <div class="posts-titulo" >Look - Jardim Botânico</div>                
+            <div class="posts">               
+               <p>Olá pessoal, meu primeiro post uhul!
+                Estou muito animada com o projeto
+                do blog! Tenho muitas ideias legais,
+                divertidas, e muito loucas para mostrar
+                a vocês, espero que curtam.</p>
+                <img src="imagens/posts/fotos/photo1.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>                
+                <p>Então vamos começar...No último final de 
+                semana fui ao Jardim Botânico, adoro esse
+                parque, e quando sai um solzinho por aqui
+                tem que aproveitar ao máximo, pois Curitiba
+                é literalmente uma cidade bipolar hehe.
+                Então resolvi vestir uma batinha bem
+                fresquinha, uma calça detonada e meu
+                querido all star.</p>                
+                <img src="imagens/posts/fotos/photo2.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
+                <img src="imagens/posts/fotos/photo3.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
+                <img src="imagens/posts/fotos/photo4.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
+                <img src="imagens/posts/fotos/photo5.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
+                <img src="imagens/posts/fotos/photo6.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
+                <img src="imagens/posts/fotos/photo7.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
+                <img src="imagens/posts/fotos/photo8.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
+                <img src="imagens/posts/fotos/photo9.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
+                <img src="imagens/posts/fotos/photo10.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
+                <img src="imagens/posts/fotos/photo11.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
+                <img src="imagens/posts/fotos/photo12.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
+                <p>Croqui - Reprodução do look Jardim Botânico</p>
+                <img src="imagens/posts/fotos/photo13.jpg" alt="Croqui - Look Jardim Botânico" title="Croqui - Look Jardim Botânico"/>                
+                <p>Bom galera é isso, vou compartilhar um
+                pouco do meu mundo com vocês, das
+                coisas que eu amo e curto em fazer.
+                Espero que gostem e até o próximo post!</p>                
+                <div class="rodape-posts"></div>                
+                <div class="paginacao"><p><a href="index.php">01</a> - <a href="pagina2.php">02</a></p></div>
+                <div class="fb-like" data-href="http://desenhandomoda.com.br/" data-layout="standard" data-action="like" data-show-faces="true" data-share="true" style="margin-top: 40px;"></div>                
+            </div>     
+            <!--<div class="fb-like" data-href="http://desenhandomoda.com.br/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>-->
+            <div class="fb-comments" data-href="http://desenhandomoda.com.br/" data-numposts="1" data-colorscheme="light" data-width="640"></div>
+        </section>        
+        <div id="section_home"></div>
+        <?php include './rodape.php';?>
+    </body>
+</html>
+
+
