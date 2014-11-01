eed67ec63341fc4d6c6930eec02bfaf3ec6d04df
helderwebergayer@gmail.com <helderwebergayer@gmail.com@fe378a3e-3d8b-3946-376f-2a8003a5fb41>
2014-11-01 02:10:40 +0000

    Post 5

--- Sheila/index.php
+++ Sheila/index.php
@@ -21,6 +21,42 @@ and open the template in the editor.
             </section> -->                   
         <section class="wrapper">                       
             <div class="broche"></div>   
+            <div class="posts-titulo5">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Books – Os Heróis do Olimpo</div>
+            <div class="posts">
+                <p>Olá pessoas, belezinha?!<br>
+                Quem curtiu Percy Jackson e os Olimpianos com certeza acompanha agora - Os Heróis do Olimpo. Neste mês de Outubro saiu o último livro da série: 
+                O Sangue do Olimpo. A série está na minha lista de livros para leitura, não tive tempo de ler ainda, pois estou terminando a série dos Jogos Vorazes, 
+                estou na parte III do livro: A Esperança e pretendo termina-lo antes da estreia do filme, quem faz coleção sabe como é difícil ler tudo de uma vez só 
+                hehe, então estou indo com calma, mas a cada livro finalizado desta série, vou fazer resenha e dar a minha opinião aqui no blog, aguardem!<br></p>                
+                <p><br>O HERÓI PERDIDO</p>
+                <img src="imagens/posts/fotos/photo30.JPG" alt="Books – Os Heróis do Olimpo" title="Books – Os Heróis do Olimpo"/>                                
+                <p><a href="http://www.osheroisdoolimpo.com.br/livro/o-heroi-perdido/" target="_blank" style="text-decoration: none;">Sinopse</a><br>                
+                <br>O FILHO DE NETUNO</p>
+                <img src="imagens/posts/fotos/photo31.JPG" alt="Books – Os Heróis do Olimpo" title="Books – Os Heróis do Olimpo"/>    
+                <p><a href="http://www.osheroisdoolimpo.com.br/livro/o-filho-de-netuno/" target="_blank" style="text-decoration: none;">Sinopse</a><br>                
+                <br>A MARCA DE ATENA</p>
+                <img src="imagens/posts/fotos/photo32.JPG" alt="Books – Os Heróis do Olimpo" title="Books – Os Heróis do Olimpo"/>    
+                <p><a href="http://www.osheroisdoolimpo.com.br/livro/a-marca-de-atena/" target="_blank" style="text-decoration: none;">Sinopse</a><br>                
+                <br>A CASA DE HADES</p>
+                <img src="imagens/posts/fotos/photo33.JPG" alt="Books – Os Heróis do Olimpo" title="Books – Os Heróis do Olimpo"/>    
+                <p><a href="http://www.osheroisdoolimpo.com.br/livro/a-casa-de-hades/" target="_blank" style="text-decoration: none;">Sinopse</a><br>
+                <br>O SANGUE DO OLIMPO</p>
+                <img src="imagens/posts/fotos/photo34.JPG" alt="Books – Os Heróis do Olimpo" title="Books – Os Heróis do Olimpo"/>    
+                <p><a href="http://www.osheroisdoolimpo.com.br/livro/o-sangue-do-olimpo/" target="_blank" style="text-decoration: none;">Sinopse</a><br>
+                <br>COLEÇÃO COMPLETA</p>
+                <img src="imagens/posts/fotos/photo35.JPG" alt="Books – Os Heróis do Olimpo" title="Books – Os Heróis do Olimpo"/> 
+                <img src="imagens/posts/fotos/photo36.JPG" alt="Books – Os Heróis do Olimpo" title="Books – Os Heróis do Olimpo"/> 
+                <img src="imagens/posts/fotos/photo37.JPG" alt="Books – Os Heróis do Olimpo" title="Books – Os Heróis do Olimpo"/>    
+                <p> Estou tentando fechar a minha coleção de marcadores desta série, se alguém estiver interessado em trocar marcadores de página, me adiciona no instagram.<br></p>
+                <div style="text-align: center;"> 
+                    <a href="http://instagram.com/sheetaylor" target="_blank"><img src="imagens/icone_instagram.png" /></a>
+                </div>                
+                <p>Ahhhhh, pra galera que tiver blog... deixe nos comentários o endereço, quero  visitar ok?! Bom galera, por hoje é só, até o próximo post.</p>
+                <div class="rodape-posts"></div>
+                <div class="fb-like" data-href="http://desenhandomoda.com.br/" data-layout="standard" data-action="like" data-show-faces="true" data-share="true" style="margin-top: 40px;"></div>                
+            </div>
+            <div class="fb-comments" data-href="http://desenhandomoda.com.br/" data-numposts="1" data-colorscheme="light" data-width="640"></div>
+            
             <div class="posts-titulo4" >Séries</div>
             <div class="posts">
                 <p>Olá pessoas, tudo bem?<br>
@@ -89,64 +125,13 @@ and open the template in the editor.
             </div>
             <div class="fb-comments" data-href="http://desenhandomoda.com.br/" data-numposts="1" data-colorscheme="light" data-width="640"></div>
             
-            <div class="posts-titulo3" >Jardineira</div>
-            <div class="posts">
-                <img src="imagens/posts/fotos/photo17.jpg" alt="Jardineira" title="Jardineira"/>    
-                <p>Olá pessoas, tudo bem?<br>
-                Olha quem voltou direto dos anos 90... Adoooro essa época... Um simples uniforme de operário, que acabou virando moda 
-                na década de 90 está voltando com tudo para a próxima estação. Já usei muuuito e curto tanto no modelo em calça e shorts.<br>
-                A jardineira com uma camiseta básica e um tênis estiloso, é a minha combinação favorita, o look fica confortável e despojado, 
-                e já que o look fica no básico é nos acessórios que vai dar um toque final. Fica a dica :)<br>
-                Separei alguns exemplos:</p>                
-                <img src="imagens/posts/fotos/photo18.jpg" alt="Jardineira" title="Jardineira"/>  
-                <p>Camiseta colorida, estampada, camisa, tricô, blusa, cropped, blazer, sapato, bota, tênis, sapatilha, alpargata, sandália e 
-                rasteirinha. Depende de você e seus acessórios, que vão completa-la, com um estilo mais despojado, ou mais sofisticado.</p>
-                <img src="imagens/posts/fotos/photo19.jpg" alt="Jardineira" title="Jardineira"/>    
-                <p>Cores, texturas e modelagem, o diferencial!</p>
-                <img src="imagens/posts/fotos/photo20.jpg" alt="Jardineira" title="Jardineira"/> 
-                <p>Estampado e divertido, invista com peças mais básicas.</p>
-                <img src="imagens/posts/fotos/photo21.jpg" alt="Jardineira" title="Jardineira"/>  
-                <p>Por hoje é isso pessoal, até o próximo post!</p>                
-                <div class="rodape-posts"></div>
-                <div class="fb-like" data-href="http://desenhandomoda.com.br/" data-layout="standard" data-action="like" data-show-faces="true" data-share="true" style="margin-top: 40px;"></div>                
-            </div>               
-            <div class="fb-comments" data-href="http://desenhandomoda.com.br/" data-numposts="1" data-colorscheme="light" data-width="640"></div>
-            
-            <div class="posts-titulo2" >Slash - World On Fire</div>                
-            <div class="posts">               
-                <p>Olá pessoas, tudo bem?<br>
-                Bom, sabe aquele álbum que teu artista favorito anuncia que em breve estará pronto?! 
-                Pois é foi uma luta e pura ansiedade esperar pelo novo álbum do Slash. 
-                Sou do tipo de fã que sempre espera que o artista me surpreenda ainda mais pelo seu 
-                último trabalho, e quem não espera não é?! E não são todos que conseguem arrancar os meus suspiros...</p>                
-               <img src="imagens/posts/fotos/photo14.jpg" alt="Slash - World On Fire" title="Slash - World On Fire"/>    
-               <p>No dia 15 de setembro foi lançado o terceiro álbum do Slash – World On Fire, com o queridíssimo 
-                Myles Kennedy que assumiu o vocal com categoria e The Conspirators com o Todd Kerns no baixo e Brent 
-                Fitz na bateria. Uma união que espero que dure por um longo tempo, pois os 4 juntos conseguiram fazer a coisa dar certo!</p>
-               <iframe width="600" height="400" src="//www.youtube.com/embed/Z0KrbsvbxtE" frameborder="0" allowfullscreen></iframe>
-               <p>O que dizer do World On Fire? Gente que categoria, o álbum tem cerca de 17 faixas, que para alguns pode ser uma coisa 
-                exaustiva de se ouvir, ou para outros como eu enlouqueceram na quantidade de músicas que o álbum tinha, pois ñ poderia 
-                esperar menos, o álbum é sensacional, as músicas estão bem trabalhadas, riffs, bateria, a técnica, melodia, é um álbum limpo, 
-                gostoso de ouvir e curtir, Slash e seus companheiros de banda se superaram! E vamos ser sinceros, hoje em dia é muito difícil ouvir coisa boa!</p>               
-               <p style="width: 600px; text-align: center; margin-top: 20px; margin-bottom: 0px;">Minha nota sobre o álbum = 9.</p>               
-               <img src="imagens/posts/elementos/maozinha3.png" style="margin-top: 0px; text-align: center; margin-left: 150px;"/>    
-               <p>Como de costume, já separei as minhas favoritas: Shadow life, Wicked stone, Bent to fly, Too far gone, Beneath the savage sun, e a Dirty girl. São muito boas *-*</p>
-               <img src="imagens/posts/fotos/photo15.jpg" alt="Slash - World On Fire" title="Slash - World On Fire"/>    
-               <p>O álbum é simplesmente viciante, e fazia tempo que eu não me animava tanto com um trabalho desse nível! Agora é esperar os caras darem uma passada pelo Brasil, 
-                e que venham logo, pois a menina aqui não é nem um pouco ansiosa haha, se o álbum já é bom, imagina o show ao vivo, né meu povo?!</p>
-               <img src="imagens/posts/fotos/photo16.jpg" alt="Slash - World On Fire" title="Slash - World On Fire"/>                                                               
-                <p>Por hoje é isso pessoal, até o próximo post!</p>                
-                <div class="rodape-posts"></div>
-                <div class="fb-like" data-href="http://desenhandomoda.com.br/" data-layout="standard" data-action="like" data-show-faces="true" data-share="true" style="margin-top: 40px;"></div>                
-            </div>     
-            <!--<div class="fb-like" data-href="http://desenhandomoda.com.br/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>-->
-            <div class="fb-comments" data-href="http://desenhandomoda.com.br/" data-numposts="1" data-colorscheme="light" data-width="640"></div>
             <div class="posts2">                
                 <div class="paginacao-titulo"><p>Posts anteriores</p></div>   
                 <div class="paginacao">                    
                     <ul>
                         <li><a href="index.php">01</a></li>
                         <li><a href="pagina2.php">02</a></li>
+                        <li><a href="pagina3.php">03</a></li>
                     </ul>                    
                 </div>
             </div>            
--- Sheila/pagina2.php
+++ Sheila/pagina2.php
@@ -20,51 +20,67 @@ and open the template in the editor.
             </div>
             </section> -->                   
         <section class="wrapper">        
-            <div class="broche"></div>   
-            <div class="posts-titulo" >Look - Jardim Botânico</div>                
+            <div class="broche"></div>
+            <div class="posts-titulo3" >Jardineira</div>
+            <div class="posts">
+                <img src="imagens/posts/fotos/photo17.jpg" alt="Jardineira" title="Jardineira"/>    
+                <p>Olá pessoas, tudo bem?<br>
+                Olha quem voltou direto dos anos 90... Adoooro essa época... Um simples uniforme de operário, que acabou virando moda 
+                na década de 90 está voltando com tudo para a próxima estação. Já usei muuuito e curto tanto no modelo em calça e shorts.<br>
+                A jardineira com uma camiseta básica e um tênis estiloso, é a minha combinação favorita, o look fica confortável e despojado, 
+                e já que o look fica no básico é nos acessórios que vai dar um toque final. Fica a dica :)<br>
+                Separei alguns exemplos:</p>                
+                <img src="imagens/posts/fotos/photo18.jpg" alt="Jardineira" title="Jardineira"/>  
+                <p>Camiseta colorida, estampada, camisa, tricô, blusa, cropped, blazer, sapato, bota, tênis, sapatilha, alpargata, sandália e 
+                rasteirinha. Depende de você e seus acessórios, que vão completa-la, com um estilo mais despojado, ou mais sofisticado.</p>
+                <img src="imagens/posts/fotos/photo19.jpg" alt="Jardineira" title="Jardineira"/>    
+                <p>Cores, texturas e modelagem, o diferencial!</p>
+                <img src="imagens/posts/fotos/photo20.jpg" alt="Jardineira" title="Jardineira"/> 
+                <p>Estampado e divertido, invista com peças mais básicas.</p>
+                <img src="imagens/posts/fotos/photo21.jpg" alt="Jardineira" title="Jardineira"/>  
+                <p>Por hoje é isso pessoal, até o próximo post!</p>                
+                <div class="rodape-posts"></div>
+                <div class="fb-like" data-href="http://desenhandomoda.com.br/" data-layout="standard" data-action="like" data-show-faces="true" data-share="true" style="margin-top: 40px;"></div>                
+            </div>               
+            <div class="fb-comments" data-href="http://desenhandomoda.com.br/" data-numposts="1" data-colorscheme="light" data-width="640"></div>
+            
+            <div class="posts-titulo2" >Slash - World On Fire</div>                
             <div class="posts">               
-               <p>Olá pessoal, meu primeiro post uhul!
-                Estou muito animada com o projeto
-                do blog! Tenho muitas ideias legais,
-                divertidas, e muito loucas para mostrar
-                a vocês, espero que curtam.</p>
-                <img src="imagens/posts/fotos/photo1.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>                
-                <p>Então vamos começar...No último final de 
-                semana fui ao Jardim Botânico, adoro esse
-                parque, e quando sai um solzinho por aqui
-                tem que aproveitar ao máximo, pois Curitiba
-                é literalmente uma cidade bipolar hehe.
-                Então resolvi vestir uma batinha bem
-                fresquinha, uma calça detonada e meu
-                querido all star.</p>                
-                <img src="imagens/posts/fotos/photo2.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
-                <img src="imagens/posts/fotos/photo3.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
-                <img src="imagens/posts/fotos/photo4.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
-                <img src="imagens/posts/fotos/photo5.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
-                <img src="imagens/posts/fotos/photo6.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
-                <img src="imagens/posts/fotos/photo7.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
-                <img src="imagens/posts/fotos/photo8.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
-                <img src="imagens/posts/fotos/photo9.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
-                <img src="imagens/posts/fotos/photo10.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
-                <img src="imagens/posts/fotos/photo11.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
-                <img src="imagens/posts/fotos/photo12.JPG" alt="Look - Jardim Botânico" title="Look - Jardim Botânico"/>
-                <p>Croqui - Reprodução do look Jardim Botânico</p>
-                <img src="imagens/posts/fotos/photo13.jpg" alt="Croqui - Look Jardim Botânico" title="Croqui - Look Jardim Botânico"/>                
-                <p>Bom galera é isso, vou compartilhar um
-                pouco do meu mundo com vocês, das
-                coisas que eu amo e curto em fazer.
-                Espero que gostem e até o próximo post!</p>                
+                <p>Olá pessoas, tudo bem?<br>
+                Bom, sabe aquele álbum que teu artista favorito anuncia que em breve estará pronto?! 
+                Pois é foi uma luta e pura ansiedade esperar pelo novo álbum do Slash. 
+                Sou do tipo de fã que sempre espera que o artista me surpreenda ainda mais pelo seu 
+                último trabalho, e quem não espera não é?! E não são todos que conseguem arrancar os meus suspiros...</p>                
+               <img src="imagens/posts/fotos/photo14.jpg" alt="Slash - World On Fire" title="Slash - World On Fire"/>    
+               <p>No dia 15 de setembro foi lançado o terceiro álbum do Slash – World On Fire, com o queridíssimo 
+                Myles Kennedy que assumiu o vocal com categoria e The Conspirators com o Todd Kerns no baixo e Brent 
+                Fitz na bateria. Uma união que espero que dure por um longo tempo, pois os 4 juntos conseguiram fazer a coisa dar certo!</p>
+               <iframe width="600" height="400" src="//www.youtube.com/embed/Z0KrbsvbxtE" frameborder="0" allowfullscreen></iframe>
+               <p>O que dizer do World On Fire? Gente que categoria, o álbum tem cerca de 17 faixas, que para alguns pode ser uma coisa 
+                exaustiva de se ouvir, ou para outros como eu enlouqueceram na quantidade de músicas que o álbum tinha, pois ñ poderia 
+                esperar menos, o álbum é sensacional, as músicas estão bem trabalhadas, riffs, bateria, a técnica, melodia, é um álbum limpo, 
+                gostoso de ouvir e curtir, Slash e seus companheiros de banda se superaram! E vamos ser sinceros, hoje em dia é muito difícil ouvir coisa boa!</p>               
+               <p style="width: 600px; text-align: center; margin-top: 20px; margin-bottom: 0px;">Minha nota sobre o álbum = 9.</p>               
+               <img src="imagens/posts/elementos/maozinha3.png" style="margin-top: 0px; text-align: center; margin-left: 150px;"/>    
+               <p>Como de costume, já separei as minhas favoritas: Shadow life, Wicked stone, Bent to fly, Too far gone, Beneath the savage sun, e a Dirty girl. São muito boas *-*</p>
+               <img src="imagens/posts/fotos/photo15.jpg" alt="Slash - World On Fire" title="Slash - World On Fire"/>    
+               <p>O álbum é simplesmente viciante, e fazia tempo que eu não me animava tanto com um trabalho desse nível! Agora é esperar os caras darem uma passada pelo Brasil, 
+                e que venham logo, pois a menina aqui não é nem um pouco ansiosa haha, se o álbum já é bom, imagina o show ao vivo, né meu povo?!</p>
+               <img src="imagens/posts/fotos/photo16.jpg" alt="Slash - World On Fire" title="Slash - World On Fire"/>                                                               
+                <p>Por hoje é isso pessoal, até o próximo post!</p>                
                 <div class="rodape-posts"></div>
                 <div class="fb-like" data-href="http://desenhandomoda.com.br/" data-layout="standard" data-action="like" data-show-faces="true" data-share="true" style="margin-top: 40px;"></div>                
             </div>     
             <!--<div class="fb-like" data-href="http://desenhandomoda.com.br/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>-->
             <div class="fb-comments" data-href="http://desenhandomoda.com.br/" data-numposts="1" data-colorscheme="light" data-width="640"></div>            
+                      
             <div class="posts2">                
                 <div class="paginacao-titulo"><p>Posts anteriores</p></div>   
                 <div class="paginacao">                    
                     <ul>
                         <li><a href="index.php">01</a></li>
                         <li><a href="pagina2.php">02</a></li>
+                        <li><a href="pagina3.php">03</a></li>
                     </ul>                    
                 </div>
             </div>            
